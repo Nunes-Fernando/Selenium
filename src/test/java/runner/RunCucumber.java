809ed04bcf66baed9d9a72a98cd6feda933552ca
Fernando <fn4586@gmail.com>
2024-12-22 11:30:03 -0300

    first steps

---
+++ src/test/java/runner/RunCucumber.java
@@ -0,0 +1,27 @@
+package runner;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty",
+		"html:target/cucumber-reports" }, features = "src/test/resources/features", glue = { "steps" })
+public class RunCucumber {
+
+	public static ChromeDriver browser;
+	
+	@BeforeClass
+	public static void start() {
+		browser = new ChromeDriver();
+	}
+
+	@AfterClass
+	public static void stop() {
+		browser.quit();
+	}
+}
---
+++ target/test-classes/features/desconto.feature
@@ -0,0 +1,17 @@
+# language: pt
+
+Funcionalidade: Receber desconto da qazando
+	Eu como usuário da qazando
+	quero receber o cupom de desconto
+	para comprar um curso com o valor reduzido
+	
+	Cenário: Visualizar o código de desconto
+		Dado que estou no site da qazando
+		Quando preencho meu e-mail 
+		E clico em ganhar cupom
+		Então visuliza o código de desconto
+	
+	
+	
+
+	
